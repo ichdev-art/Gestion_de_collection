c725ff24a977269a6033b5847e4f81044582b617
Ichem <ichem76610@hotmail.com>
2025-03-25 22:14:06 +0100

    recuperation de certain fichier

---
+++ templates/nav.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="nav-title">
+        <a href="#">
+            <b>MANGA</b><span>THEQUE</span>
+        </a>
+    </div>
+    <div class="nav-user">
+        <a href="#" class="user">
+            <i class="fa-solid fa-user"></i>
+        </a>
+        <a href="#" class="profile">
+            <i class="fa-solid fa-book"></i>
+        </a>
+    </div>
+</nav>
