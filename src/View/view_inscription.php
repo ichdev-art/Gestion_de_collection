47840a67f10ee562bd426f709bd27870c63fce37
IchemJ <ichem76610@hotmail.com>
2025-03-26 10:37:47 +0100

    ajout de données dans la bdd

--- src/View/view_inscription.php
+++ src/View/view_inscription.php
@@ -13,27 +13,17 @@
             <div class="mb-3">
               <label for="email" class="form-label">E-mail</label>
               <input type="email" class="form-control" id="email" name="email">
-<<<<<<< HEAD
               <div class="form-error"><?= $error['email'] ?? '' ?></div>
-=======
-              <div class="form-error">ce champ est obligatoire</div>
->>>>>>> master
             </div>
             <div class="mb-3">
                 <label for="pseudo" class="form-label">Pseudo</label>
                 <input type="text" class="form-control" id="pseudo" name="pseudo">
-<<<<<<< HEAD
                 <div class="form-error"><?= $error['pseudo'] ?? '' ?></div>
-=======
->>>>>>> master
               </div>
             <div class="mb-3">
               <label for="password" class="form-label">Mot de passe</label>
               <input type="password" class="form-control" id="password" name="password">
-<<<<<<< HEAD
               <div class="form-error"><?= $error['password'] ?? '' ?></div>
-=======
->>>>>>> master
             </div>
             <button type="submit">Envoyer</button>
           </form>
@@ -41,8 +31,4 @@
     </section>
 </body>
 
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> master
--- src/View/view_profil.php
+++ src/View/view_profil.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
             <button>
                 <a href="../Controller/controller_deconnexion.php" class="text"><i class="fa-solid fa-right-from-bracket"></i>Déconnexion</a>
             </button
-=======
-<?php include_once '../../templates/head.php' ?>
->>>>>>> master
